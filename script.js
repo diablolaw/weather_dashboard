ae8410273ae8e2e00438affe9c6120b5cd0f9fa4
Diablolaw <94478511+diablolaw@user.noreply.github.com>
2022-01-31 01:17:47 +0000

    forecast update

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ function currentWeather(city) {
       $(currentWSpeed).html(windsmph + "MPH");
 
       UVIndex(data.coord.lon, data.coord.lat);
+      forecast(data.id);
     });
 }
 function UVIndex(ln, lt) {
@@ -63,6 +64,36 @@ function UVIndex(ln, lt) {
     })
     .then(function (data) {
       $(currentUvindex).html(data.value);
+    });
+}
+
+function forecast(cityid) {
+  var dayover = false;
+  var queryforcastURL =
+    "https://api.openweathermap.org/data/2.5/forecast?id=" +
+    cityid +
+    "&appid=" +
+    APIKey;
+  fetch(queryforcastURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      for (i = 0; i < 5; i++) {
+        var date = new Date(
+          response.list[(i + 1) * 8 - 1].dt * 1000
+        ).toLocaleDateString();
+        var iconcode = response.list[(i + 1) * 8 - 1].weather[0].icon;
+        var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+        var tempK = response.list[(i + 1) * 8 - 1].main.temp;
+        var tempF = ((tempK - 273.5) * 1.8 + 32).toFixed(2);
+        var humidity = response.list[(i + 1) * 8 - 1].main.humidity;
+
+        $("#fDate" + i).html(date);
+        $("#fImg" + i).html("<img src=" + iconurl + ">");
+        $("#fTemp" + i).html(tempF + "&#8457");
+        $("#fHumidity" + i).html(humidity + "%");
+      }
     });
 }
 
